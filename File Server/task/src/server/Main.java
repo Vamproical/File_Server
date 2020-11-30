ef6cae23c8d559825e1b84a26ea11c20792ff2af
Nick <mimoun136@yandex.ru>
2020-11-30 16:29:13 +1000

    Complete first stage.

--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -6,6 +6,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       
+        List<String> files = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("exit")) {
+            String[] inputs = input.split(" ");
+            switch (inputs[0]) {
+                case "add":
+                    if (files.contains(inputs[1]) || (!inputs[1].matches("file\\d|file10"))) {
+                        System.out.println("Cannot add the file " + inputs[1]);
+                    } else {
+                        files.add(inputs[1]);
+                        System.out.println("The file " + inputs[1] + " added successfully");
+                    }
+                    break;
+                case "get":
+                    if (files.contains(inputs[1])) {
+                        System.out.println("The file " + inputs[1] + " was sent");
+                    } else {
+                        System.out.println("The file " + inputs[1] + " not found");
+                    }
+                    break;
+                case "delete":
+                    if (files.contains(inputs[1])) {
+                        files.remove(inputs[1]);
+                        System.out.println("The file " + inputs[1] + " was deleted");
+                    } else {
+                        System.out.println("The file " + inputs[1] + " not found");
+                    }
+                    break;
+            }
+            input = scanner.nextLine();
+        }
     }
 }
