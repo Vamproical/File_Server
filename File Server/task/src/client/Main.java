86a5efa506ac6c4f72a4e721f6902a77f45e4ec5
Nick <mimoun136@yandex.ru>
2020-12-20 14:05:15 +1000

    Complete the third stage.

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -5,21 +5,80 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
+    private static boolean isPost = false;
+    private static boolean isGet = false;
+    private static boolean isDelete = false;
+    private final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         String address = "127.0.0.1";
         int port = 23456;
-        System.out.println("Client started!");
         try (
                 Socket socket = new Socket(InetAddress.getByName(address), port);
                 DataInputStream input = new DataInputStream(socket.getInputStream());
                 DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
-            System.out.println("Sent: Give me everything you have!");
-            output.writeUTF("Give me everything you have!");
-            System.out.println("Received: " + input.readUTF());
+            String request = createRequest();
+            System.out.println("The request was sent.");
+            output.writeUTF(request);
+            if (!request.equals("exit")) {
+                String response = input.readUTF();
+                processResponse(response);
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void processResponse(String response) {
+        if (isPost) {
+            if (response.contains("403")) {
+                System.out.println("The response says that creating the file was forbidden!");
+            } else {
+                System.out.println("The response says that the file was created!");
+            }
+        } else if (isGet) {
+            if (response.contains("404")) {
+                System.out.println("The response says that the file was not found!");
+            } else {
+                System.out.println("The content of the file is: " + response.substring(4));
+            }
+        } else if (isDelete) {
+            if (response.contains("404")) {
+                System.out.println("The response says that the file was not found!");
+            } else {
+                System.out.println("The response says that the file was successfully deleted!");
+            }
+        }
+    }
+
+    private static String createRequest() {
+        System.out.println("Enter action (1 - get the file, 2 - create a file, 3 - delete the file):");
+        String action = scanner.next();
+        switch (action) {
+            case "1":
+                System.out.println("Enter filename: ");
+                String filename = scanner.next();
+                scanner.nextLine();
+                isGet = true;
+                return "GET " + filename;
+            case "2":
+                System.out.println("Enter filename: ");
+                String file = scanner.next();
+                scanner.nextLine();
+                System.out.println("Enter file content: ");
+                String content = scanner.nextLine().trim();
+                isPost = true;
+                return "PUT " + file + " " + content;
+            case "3":
+                System.out.println("Enter filename: ");
+                String name = scanner.next();
+                scanner.nextLine();
+                isDelete = true;
+                return "DELETE " + name;
+            default:
+                return "exit";
         }
     }
 }
--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -1,27 +1,86 @@
 package server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String address = "127.0.0.1";
         int port = 23456;
         System.out.println("Server started!");
-        try (
-        ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address));
-        Socket socket = server.accept();
-        DataInputStream input = new DataInputStream(socket.getInputStream());
-        DataOutputStream output  = new DataOutputStream(socket.getOutputStream())) {
-            System.out.println("Received: " + input.readUTF());
-            System.out.println("Sent: All files were sent!");
-            output.writeUTF("All files were sent!");
+        boolean isRun = true;
+        while (isRun)
+            try (
+                    ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address));
+                    Socket socket = server.accept();
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+                    DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+                String response = input.readUTF();
+                if (response.equals("exit")) {
+                    isRun = false;
+                } else {
+                    String request = processResponse(response);
+                    assert request != null;
+                    output.writeUTF(request);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+
+    private static String processResponse(String response) {
+        String path = "C:\\Users\\Никита\\IdeaProjects\\File Server\\File Server\\task\\src\\server\\data";
+        String[] data = response.split(" ");
+        File file = new File(path + "\\" + data[1]);
+        if (response.startsWith("GET")) {
+            if (!isCreated(file)) {
+                return "404";
+            } else {
+                try (Scanner scanner = new Scanner(file)) {
+                    return "200 " + scanner.nextLine();
+                } catch (FileNotFoundException e) {
+                    System.out.println("No file found: " + path);
+                }
+            }
+        } else if (response.startsWith("PUT")) {
+            if (isCreated(file)) {
+                return "403";
+            } else {
+                try (FileWriter writer = new FileWriter(file)) {
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 2; i < data.length; i++) {
+                        builder.append(data[i]).append(" ");
+                    }
+                    writer.write(builder.toString().trim());
+                    return "200";
+                } catch (IOException e) {
+                    System.out.printf("An exception occurs %s", e.getMessage());
+                }
+            }
+        } else {
+            if (!isCreated(file)) {
+                return "404";
+            } else {
+                if (file.delete()) {
+                    return "200";
+                } else {
+                    return "404";
+                }
+
+            }
+        }
+        return null;
+    }
+
+    private static boolean isCreated(File file) {
+        try {
+            return !(file.createNewFile());
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Cannot create the file: " + file.getPath());
+            return false;
         }
     }
 }
